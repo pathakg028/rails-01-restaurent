8d97f2486e1817ec773571158a3963de809f9397
Gaurav Pathak <gauravpathak@Gauravs-MacBook-Pro-2.local>
2019-04-17 17:39:29 +1000

    restaurant running

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get "/restaurants/new", to: "restaurants#new", as: "new_restaurant"
   get "/restaurants/:id", to: "restaurants#show", as: "restaurant"
 
+  put "/restaurants/:id", to: "restaurants#update"
+  patch "/restaurants/:id", to: "restaurants#update"
+  get "/restaurants/:id/edit", to: "restaurants#edit", as: "edit_restaurant"
+  post "/reviews", to: "reviews#create", as: "review"
   root 'restaurants#index'
 end
